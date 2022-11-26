d72c0f31b22004c386d8474e8a1a7a7f82103de7
ompanchwate2003 <ompanchwate2003@gmail.com>
2022-11-26 13:29:33 +0530

    Assignment_1 done

--- OOP_Assignment_1.cpp
+++ OOP_Assignment_1.cpp
@@ -1,4 +1,7 @@
-/*Implement a class Complex which represents the Complex Number data type. 
+/*------ Assignment no. 1 --------
+-----Author : Om G. Panchwate-----
+
+Implement a class Complex which represents the Complex Number data type. 
 Implement the following 
 1. Constructor (including a default constructor which creates the complex number 0+0i). 
 2. Overload operator+ to add two complex numbers. 
